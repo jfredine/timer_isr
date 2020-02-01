82f68e0b8767aa0bd822d3c847e4a639dc0c6bc8
John Fredine <jfredine@hotmail.com>
2020-01-31 20:56:39 -0800

    Use compare register to generate interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -2,66 +2,66 @@
 
 volatile long unsigned total_isr_count;
 
-#define USE_TIMER0
+// time between interrupts for monitoring signal values
+// resolution of the timer is 4us so make this >= 4 and a multiple of 4
+#define ISR_INTERVAL_us 256
 
-#if defined(USE_TIMER2)
-ISR(TIMER2_COMPA_vect) {
-#else
+//******************************************************************************
+// calculated parameters which should not need user adjustment
+
+// Number of timer ticks between interrupts.  Each tick is 4us
+#define OCR0A_INCR (ISR_INTERVAL_us / 4)
+
+/*
+ * TIMER0_COMPA_vect
+ * ISR triggered by timer0 where inputs are read and debounced.
+ * In order to get interrupts at a rate greater than 1 ms (default for timer0),
+ * but not require an additional timer and not disturb the timer0 overflow rate
+ * (because that is used by default libraries), it makes use of the timer
+ * compare interrupt and adjusts the comparison value within the ISR.
+ */
+
 ISR(TIMER0_COMPA_vect) {
-#endif
-    total_isr_count++;
-#if defined(USE_TIMER2)
-    OCR2A = (OCR2A + 64) & 0xff;
-#else
-    OCR0A = (OCR0A + 64) & 0xff;
-#endif
-}
 
+    total_isr_count++;
+
+    // set the time of the next interrupt
+    OCR0A = (OCR0A + OCR0A_INCR) & 0xff;
+
+    // set up a recuring timer interrupt
+    cli();  // disable interrupts
+
+    // Turn off PWM modes of timer0 because such modes prevent the
+    // immediate re-assignment of compare values to OCR0A
+    TCCR0A &= ~((1 << WGM01) | (1 << WGM00));
+    TCCR0B &= ~(1 << WGM02);
+
+    OCR0A = 1;              // Set the timer0 compare value.
+                            // The ISR will adjust this value to to potentially
+                            // produce multiple interrupts during the normal
+                            // 0 -> FF count sequence which takes 1024us
+    TIMSK0 |= _BV(OCIE0A);  // Enable the timer compare interrupt
+
+    sei();  // enable interrupts
+}
 
 void setup() {
     char buffer[15];
     Serial.begin(9600);
 
+    // set up a recuring timer interrupt
     cli();  // disable interrupts
 
-    //set timer to run at 256KHz and interrupt on comparison match
-#if defined(USE_TIMER2)
-    TCCR2A = 0;
-    TCCR2B = 0;
-    TCNT2 = 0;
-
-    TCCR2B |= (1 << CS22);   // clock prescaling of /64
-    OCR2A = 1;               // counter compare A value
-    TIMSK2 |= _BV(OCIE2A);   // set the compare A output to cause an interrupt
-
-    snprintf(buffer, sizeof(buffer), "TCCR2A = 0x%02x", TCCR2A);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "TCCR2B = 0x%02x", TCCR2B);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "OCR2A = 0x%02x", OCR2A);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "OCR2B = 0x%02x", OCR2B);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "TIMSK2 = 0x%02x", TIMSK2);
-    Serial.println(buffer);
-#else
-    TCCR0A &= ~((1 << WGM01) | (1 << WGM00));  // Turn off PWM modes
+    // Turn off PWM modes of timer0 because such modes prevent the
+    // immediate re-assignment of compare values to OCR0A
+    TCCR0A &= ~((1 << WGM01) | (1 << WGM00));
     TCCR0B &= ~(1 << WGM02);
 
-    OCR0A = 1;               // counter compare A value
-    TIMSK0 |= _BV(OCIE0A);   // set the compare A output to cause an interrupt
-
-    snprintf(buffer, sizeof(buffer), "TCCR0A = 0x%02x", TCCR0A);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "TCCR0B = 0x%02x", TCCR0B);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "OCR0A = 0x%02x", OCR0A);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "OCR0B = 0x%02x", OCR0B);
-    Serial.println(buffer);
-    snprintf(buffer, sizeof(buffer), "TIMSK0 = 0x%02x", TIMSK0);
-    Serial.println(buffer);
-#endif
+    OCR0A = 1;              // Set the timer0 compare value.
+                            // The ISR will adjust this value to to potentially
+                            // produce multiple interrupts during the normal
+                            // 0 -> FF count sequence which takes 1024us
+    TIMSK0 |= _BV(OCIE0A);  // Enable the timer compare interrupt
 
     sei();  // enable interrupts
 }
